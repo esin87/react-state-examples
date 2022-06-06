c7320df4fa6ea2d9763e99f1c04c76ed785ae7dd
esin87 <esin.saribudak@gmail.com>
2022-06-06 16:24:32 -0500

    Add unfriend functionality

--- src/Friend.js
+++ src/Friend.js
@@ -1,7 +1,12 @@
-function Friend({ friend }) {
+function Friend({ friend, handleUnfriend }) {
 	// const {friend} = props;
 	// console.log(props);
-	return <li>{friend}</li>;
+	return (
+		<li>
+			{friend} -{' '}
+			<button onClick={(event) => handleUnfriend(friend)}>Unfriend</button>
+		</li>
+	);
 }
 
 export default Friend;
--- src/Friends.js
+++ src/Friends.js
@@ -3,10 +3,48 @@ import Friend from './Friend';
 
 function Friends(props) {
 	const [friends, setFriends] = useState(['Zoe', 'Tom', 'Esin', 'Ye', 'Em']);
+
+	const handleUnfriend = (friendToDelete) => {
+		// Option 1: Filter
+		const friendsToKeep = friends.filter(
+			(element) => element !== friendToDelete
+		);
+		setFriends(friendsToKeep);
+		// console.log(friendToDelete);
+		// Option 2: For Loop
+		// const newFriends = [];
+		// for (let i = 0; i < friends.length; i++) {
+		// 	if (friends[i] === friendToDelete) {
+		// 		continue;
+		// 	} else {
+		// 		newFriends.push(friends[i]);
+		// 	}
+		// }
+		// setFriends(newFriends);
+		// Option 3: indexOf/splice
+		// find the index of the friendToDelete
+		// const index = friends.indexOf(friendToDelete);
+		// // make a copy of state
+		// const temp = [...friends];
+		// console.log(index, temp);
+		// // use splice to delete the friend at that index
+		// temp.splice(index, 1);
+
+		// // use setFriends to update the state data with the copy
+		// setFriends(temp);
+	};
+
 	return (
 		<ul>
 			{friends.map((element) => {
-				return <Friend key={element} friend={element} />;
+				return (
+					<Friend
+						key={element}
+						// passing down props from this component to its child, the Friend component
+						friend={element}
+						handleUnfriend={handleUnfriend}
+					/>
+				);
 			})}
 		</ul>
 	);
